7ab98e22c68d6514821a9407c2981a747e19530e
sychus <hfernandez@neuquen.gov.ar>
2020-10-02 18:05:08 -0300

    decodificamos datos

--- index.ts
+++ index.ts
@@ -1,53 +1,55 @@
-import { AnyElement } from "soap/lib/wsdl/elements";
+import XRoad from 'xroad-client';
+const atob = require('atob');
 
-const soap = require('soap');
-const path = require('path');
-var url = path.resolve('wsdl/WS_Covid_LaboratorioCentral.wsdl');
+async function llama() {
 
-
-soap.createClient(url, function(err: any, client: any) {
-    if (err) {
-        return  console.log('Error al crear cliente:', err)
-    }
-  // para saber los serivcios que tiene consulto al cliente
-  console.log(client.describe());
-  //  
-  client.addSoapHeader({
-      'xrd:client': {
+  // Create the configuration object pointing to 
+  // the consumer security server
+  const xRoadConfig: any = {
+      securityServer: '10.1.62.67', // The security server address
+      client: {
+        xRoadInstance: 'roksnet',
+        memberClass: 'GOV',
+        memberCode: '70000101',
+        subsystemCode: 'GP-SALUD'
+      }
+  }
+  
+  // create the consumer instance
+  const consumer = new XRoad(xRoadConfig);
+  
+  // Create the request object
+  const requestConfig = {
+    secure: "false", // If true uses https instead of http 
+    service: {
       xRoadInstance: 'roksnet',
       memberClass: 'GOV',
-      memberCode: '70000101',
-      subsystemCode: 'GP-SALUD'
-    },
-      'xrd:service': {
-      xRoadInstance: 'roksnet',
-      memberClass: 'GOV',
-      memberCode: '70000101',
-      subsystemCode: 'GP-SALUD',
+      memberCode:'70000101',
+      subsystemCode:'GP-SALUD',
       serviceCode: 'WS_Covid_LaboratorioCentral',
       serviceVersion: 'v1'
     },
-    "xrd:id": 'ID11234',
-    "xrd:userId": 'EE1234567890',
-    "xrd:protocolVersion": '4.0'
-    },
-    null, null, null
-  );
-  let userId = 'AR22920765';
-  let id = '1231232';
-  let protocolVersion = '4.0';
-  let args = {userId, id, protocolVersion};
+     
+    headers: {
+      
+  },
+    body: (
+      `<WS_Covid_LaboratorioCentral xmlns="http://thinknet.x-road.eu/producer/">
+      <request xmlns="">
+      <fuenteautentica/>
+      <servicio/>
+      <involucrado>Desde=01/08/2020;Hasta=02/08/2020</involucrado>
+      </request>
+      </WS_Covid_LaboratorioCentral>
+      `
+    )
+  };
+    
+  const soapClient: any =  consumer.getSoapClient();
+  const result = await soapClient.request(requestConfig);
+  const res = (result['SOAP-ENV:Envelope']['SOAP-ENV:Body']['tns:WS_Covid_LaboratorioCentralResponse']);
+  // Con esto vemos el texto (falta poner bonito)
+  console.log(atob(res.response.resultado1));
+}
 
-
-   client.WS_Covid_LaboratorioCentral(args,function(err:any, result: any){
-    //  if (err) {
-    //    console.log('Palenque: ', err);
-    //    return err;
-    //  }
-    console.log(result);
-   })
-
-});
-
-
-
+llama();
